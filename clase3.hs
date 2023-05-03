08ddf776ce95c5fa9de61bc8a17b40fc827f6770
Fabio Mazzarella <fabiomazzarella97@hotmail.com>
2023-05-03 12:01:10 -0300

    Add clase 3 presencial

--- clase3.hs
+++ clase3.hs
@@ -1,84 +1,72 @@
+-- Pattern Matching
+
+signo :: Integer -> Integer
+signo nro
+  | nro == 0 = 0
+  | nro < 0 = -1
+  | otherwise = 1
+
+suma :: Integer -> (Integer -> Integer) -- Currificacion
+suma nro otroNum = nro + otroNum
+
+suma' :: (Integer, Integer) -> Integer
+suma' (nro, otroNum) = nro + otroNum
+
+siguiente :: Integer -> Integer
+siguiente nro = nro + 1
+
 -- Composicion de funciones
--- (.)
-
--- (>5).(3*)
-
--- even.(+1).(4*)
-
--- Currificacion
--- Aplicacion Parcial
-  -- Permite poder crear nuevas funciones
+-- :t -> es el type
+-- :t (siguiente . (suma 8))
+-- (siguiente . suma 8) :: Integer -> Integer
 
 -- Expresiones Lambda
-  -- Crear una funcion anonima (/x -> x+1)
+-- >(\nro -> nro + 1) 10
+-- 11
 
--- Tipos de datos propios
-  -- Usamos data
+type Radio = Double
+type Area = Double
+area :: Radio -> Area
 
--- Funciones constante
-  -- pedro = Persona 20 "pedro"
+area radio = pi * radio ^ 2
 
+area Rectangulo :: Base -> Altura -> Area
+area Rectangulo base altura = base * altura
 
+area::Figura -> Area
+area (circulo radio) = pi * radio ^ 2
+area (Rectangulo base altura) = bas * alt
+area (Cuadrado lado) = lado * lado
 
--- FUNCIONES
+data Bool = True | False
+data colores = Rojo | Azul | Amarillo
 
---Ejercicio 1
--- busca el primer elemento en una lista que cumple con un criterio de búsqueda específico.
--- Listas por comprension
--- find' :: (a -> Bool) -> [a] -> a
--- find' criterio lista = head [elem | elem <- lista, criterio elem]
+-- Ejercicio 1
+cuadruple :: Integer -> Integer
+cuadruple nro = (doble . doble) nro
+-- doble (doble nro)
 
-find' :: (a -> Bool) -> [a] -> a
-find' criterio lista = (head . filter criterio) lista
+-- Ejercicio 2
+esMayorA::Integer -> Bool
+esMayorA nro = ( (>10) . doble . siguiente . suma 2) nro 
+--             ((>10). doble . siguiente . (+2)) nro
 
-data Politico = Politico {proyectosPresentados :: [String], sueldo :: Integer,  edad :: Int } deriving Show 
-politicos =
-  [ 
-  Politico ["ser libres", "libre estacionamiento coches politicos", "ley no fumar", "ley 19182"] 20000 81,
-  Politico ["tratar de reconquistar luchas sociales"] 10000 63,
-  Politico ["tolerancia 100 para delitos"] 15500 49
-  ]
+-- Ejercicio 3
+triple nro = (\x -> 3*x) nro 
+siguiente nro = (\x -> x + 1) nro
+suma nro otroNum = (\x y -> x + y) nro otroNum
+sumarDos nro = (\n -> n + 2) nro 
 
-pedro :: Politico
-pedro = Politico ["tolerancia 100 para delitos"] 15500 49
+-- Ejercicio 4
+data Empleado = Comun { sueldoBasico::Double, nombre::String} | Jefe { sueldoBasico::Double, cantACargo::Int, nombre::String }
 
-{-
-a) ghci> find' ((<50).edad) politicos Politico 
-{proyectosPresentados = ["tolerancia 100 para delitos"], sueldo = 15500, edad = 49}
--} 
+type sueldo = Double
+sueldo::Empleado->sueldo
+sueldo(comun basico _ ) = basico
+sueldo(jefe basico catACargo _ ) = (basico + plus) cantACargo
+plus::Int->Double
+plus cant = cant * 1000
 
--- b)  alguno que haya presentado más de 3 proyectos
--- find' ((>3).length.proyectosPresentados) politicos
+sonia = Jefe 15000 3 "Sonia"
+pedro = comun 10000 "Pedro"
 
--- c) alguno que haya presentado algún proyecto que tenga más de 3 palabras
--- find' (any ((>3).length.words).proyectosPresentados) politicos
-
-type Nombre = String
-type Notas = [Int]
-data Persona = Alumno {nombre :: Nombre, notas :: Notas}
-
--- 2. Definir la función promediosAlumnos/1, que dada una lista de alumnos devuelve una lista de tuplas que tenga el alumno y el promedio (Consideramos la división entera para el promedio y usamos la funcion div).
-promediosAlumnos :: [Persona] -> [(Nombre, Int)]
-promediosAlumnos alumnos = map( \unAlumno -> (nombre unAlumno, (promedio.notas)unAlumno)) alumnos
-
-promedio :: Notas -> Int
-promedio notas = (sum notas) `div` (length notas)
-
--- 3. Definir la función promediosSinAplazos/1, que dada una lista de listas, devuelve la lista de los promedios de cada lista-elemento, excluyendo los que sean menores a 6 que no se cuentan.
-promediosSinAplazos :: [Notas] -> [Int]
-promediosSinAplazos listaNotas = map (promedio.filter (>=6)) listaNotas
-
--- 4. Definir la función aprobó/1, que dado un alumno devuelve True si el alumno aprobó. Aclaración: se dice que un alumno aprobó si todas sus notas son 6 o más.
-aprobo :: Persona -> Bool
-aprobo (Alumno _ notas) = all (>=6) notas
-
--- 5. Definir la función aprobaron/1, que dada una lista de alumnos, devuelve los nombres de los alumnos que aprobaron.
-aprobaron :: [Persona] -> [Nombre]
-aprobaron alumnos = (map nombre.filter aprobo) alumnos
-
--- 6. Definir la función productos que dado una lista de nombres  de productos y una lista de precios, devuelve una lista de tuplas.
-productos :: [Nombre] -> [Integer] -> [(Nombre, Integer)]
-productos nombres precios = zip nombres precios
-
-productos' :: [Nombre] -> [Integer] -> [(Nombre, Integer)]
-productos' nombres precios = zipWith (\nom prec -> (nom, prec)) nombres precios
